3b303e4d4357e538e7e8ce13ee83b41d1842437d
Simon Kornblith <simon@simonster.com>
2012-04-17 02:07:01 -0400

    Convert strings to UTF-8 before processing

--- pre.js
+++ pre.js
@@ -1,9 +1,63 @@
+/**
+ * Adds a string to a given array at a given offset, converted to UTF-8
+ * @param {String} string The string to convert to UTF-8
+ * @param {Array|Uint8Array} array The array to which to add the string
+ * @param {Integer} [offset] Offset at which to add the string
+ */
+function stringToUTF8Array(string, array, offset) {
+	if(!offset) offset = 0;
+	var n = string.length;
+	for(var i=0; i<n; i++) {
+		var val = string.charCodeAt(i);
+		if(val >= 128) {
+			if(val >= 2048) {
+				array[offset] = (val >>> 12) | 224;
+				array[offset+1] = ((val >>> 6) & 63) | 128;
+				array[offset+2] = (val & 63) | 128;
+				offset += 3;
+			} else {
+				array[offset] = ((val >>> 6) | 192);
+				array[offset+1] = (val & 63) | 128;
+				offset += 2;
+			}
+		} else {
+			array[offset++] = val;
+		}
+	}
+}
+
+/**
+ * Gets the byte length of the UTF-8 representation of a given string
+ * @param {String} string
+ * @return {Integer}
+ */
+function getStringByteLength(string) {
+	var length = 0, n = string.length;
+	for(var i=0; i<n; i++) {
+		var val = string.charCodeAt(i);
+		if(val >= 128) {
+			if(val >= 2048) {
+				length += 3;
+			} else {
+				length += 2;
+			}
+		} else {
+			length += 1;
+		}
+	}
+	return length;
+}
+
+// Emscripten file system functions
 var cslValidatorInput = null,
+	cslValidatorInputLocation = 0;
 	cslValidatorOutput = "";
 var Module = {
 	"arguments":["csl.rnc"],
 	"stdin":function() {
-		if(cslValidatorInput.length > 1) return cslValidatorInput.shift();
+		if(cslValidatorInputLocation < cslValidatorInput.length) {
+			return cslValidatorInput[cslValidatorInputLocation++];
+		}
 		return null;
 	},
 	"stdout":function(code) {
@@ -14,9 +68,11 @@ var Module = {
 	},
 	"noInitialRun":true
 };
-
 validate = function(string) {
-	cslValidatorInput = Module['intArrayFromString'](string);
+	cslValidatorInput = new Uint8Array(getStringByteLength(string));
+	stringToUTF8Array(string, cslValidatorInput);
+	cslValidatorInputLocation = 0;
+	
 	FS.streams[0] = FS.streams[1];
 	run();
 	return cslValidatorOutput;
